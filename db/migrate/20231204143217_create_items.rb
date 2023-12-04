531c62ad8dd466e8f6ddee0bcde258cccd318a33
EC2 Default User <ec2-user@ip-172-31-34-181.ap-northeast-1.compute.internal>
2023-12-04 14:46:26 +0000

    model item create

--- db/migrate/20231204143217_create_items.rb
+++ db/migrate/20231204143217_create_items.rb
@@ -4,7 +4,6 @@ class CreateItems < ActiveRecord::Migration[6.1]
       t.string :name, null: false
       t.text :introduction, null: false
       t.integer :price, null: false
-      
       t.timestamps
     end
   end
