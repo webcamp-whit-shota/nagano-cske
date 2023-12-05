181fe9c9ebd4a050d2f5c4131fc42e4d30845886
EC2 Default User <ec2-user@ip-172-31-34-181.ap-northeast-1.compute.internal>
2023-12-05 12:51:23 +0000

    Order create

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_05_123735) do
+ActiveRecord::Schema.define(version: 2023_12_05_124311) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "item_id", null: false
@@ -24,6 +24,18 @@ ActiveRecord::Schema.define(version: 2023_12_05_123735) do
     t.string "name", null: false
     t.text "introduction", null: false
     t.integer "price", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
+    t.string "address_name", null: false
+    t.integer "payment_method", null: false
+    t.integer "shipping_fee", null: false
+    t.integer "billing_amount", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
